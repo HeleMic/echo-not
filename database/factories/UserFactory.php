24c8db9a3d427c92b8dad6f53df91a53d8e2be23
Michele Lorenzoni <michelelorenzoni321@gmail.com>
2026-01-26 23:59:34 +0100

    fix(users): use 'user' as default role in factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'role' => UserRole::Admin,
+            'role' => UserRole::User,
             'remember_token' => Str::random(10),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
