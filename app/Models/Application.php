99365fb96ad34d866ac046a7c21642fc2e99eb48
Michele Lorenzoni <michelelorenzoni321@gmail.com>
2026-01-27 00:57:30 +0100

    chore(application): remove unused trait

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Application extends Model
 {
     /** @use HasFactory<\Database\Factories\ApplicationFactory> */
-    use HasFactory, HasUuids;
+    // use HasFactory,
+    use HasUuids;
 
     /**
      * The "type" of the ID.
