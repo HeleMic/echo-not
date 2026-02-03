00c6b909dba4800ce8c4b3fd990837408e57ad91
Michele Lorenzoni <michelelorenzoni321@gmail.com>
2026-02-03 23:43:56 +0100

    refactor(application): update service to use new split dtos

--- app/Services/ApplicationService.php
+++ app/Services/ApplicationService.php
@@ -4,7 +4,8 @@ namespace App\Services;
 
 use App\Models\User;
 use App\Models\Application;
-use App\DTO\ApplicationDTO;
+use App\DTO\Applications\StoreApplicationDTO;
+use App\DTO\Applications\UpdateApplicationDTO;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class ApplicationService
@@ -35,10 +36,10 @@ class ApplicationService
     /**
      * Create a new application.
      *
-     * @param  \App\DTO\ApplicationDTO $dto
+     * @param  \App\DTO\Applications\StoreApplicationDTO $dto
      * @return \App\Models\Application
      */
-    public function create(ApplicationDTO $dto): Application
+    public function create(StoreApplicationDTO $dto): Application
     {
         return Application::create([
             'user_id' => $dto->userId,
@@ -51,10 +52,10 @@ class ApplicationService
      * Update an existing application by ID.
      *
      * @param  string $id
-     * @param  \App\DTO\ApplicationDTO $dto
+     * @param  \App\DTO\Applications\UpdateApplicationDTO $dto
      * @return \App\Models\Application
      */
-    public function updateById(string $id, ApplicationDTO $dto): Application
+    public function updateById(string $id, UpdateApplicationDTO $dto): Application
     {
         $application = Application::findOrFail($id);
         return $this->update($application, $dto);
@@ -64,10 +65,10 @@ class ApplicationService
      * Update an existing application.
      *
      * @param  \App\Models\Application $application
-     * @param  \App\DTO\ApplicationDTO $dto
+     * @param  \App\DTO\Applications\UpdateApplicationDTO $dto
      * @return \App\Models\Application
      */
-    public function update(Application $application, ApplicationDTO $dto): Application
+    public function update(Application $application, UpdateApplicationDTO $dto): Application
     {
         $application->update([
             'name' => $dto->name,
